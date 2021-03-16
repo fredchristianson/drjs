8fad35934d2fe02e6359e120a4261e0cf66b01e9
Fred Christianson <fred@devrelief.com>
2021-03-16 10:19:20 -0400

    more asserts and Ensure

--- assert.js
+++ assert.js
@@ -1,16 +1,42 @@
 import Logger from '../../shared/js/logger.js';
 const log = Logger.create('Assert');
 
-export class Assert {
-    equal(val1,val2,message=null) {
-        if (val1 == val2) {
-            return;
-        }
-
-        const err = new Error(message || "assert.equal() failed");
-        log.error(err);
-        throw err;
+export class AssertionError extends Error {
+    constructor(message) {
+        super(message);
     }
 }
+export class Assert {
+    test(func,message) {
+        if (!func()) {
+            const errorMessage = message || "assert test failed";
+            console.error(errorMessage);
+            throw new AssertionError(errorMessage);
+        }
+    }
 
-export default new Assert();
+    equal(val1,val2,message=null) {
+        this.test(()=>val1==val2,message || "assert.equal() failed");
+    }
+
+    notEqual(val1,val2,message=null) {
+        this.test(()=>val1!=val2,message || "assert.notEqual() failed");
+    }
+
+    null(val1,message=null) {
+        this.test(()=>val1===null || (typeof val1 === 'undefined'),message || "assert.null() failed");
+    }
+
+    notNull(val1,message=null) {
+        this.test(()=>val1!==null && (typeof val1 !== 'undefined'),message || "assert.notNull() failed");
+    }
+
+    range(val,minValue,maxValue,message) {
+        this.test(()=>val>=minValue && val <= maxValue,message || "assert.notEqual() failed");
+    }
+
+    notRange(val,minValue,maxValue,message) {
+        this.test(()=>val<minValue || val > maxValue,message || "assert.notEqual() failed");
+    }
+
+}
